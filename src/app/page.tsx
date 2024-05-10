ba06e6114c4ed272d8e2a140037ed200734267e0
JavierCisneros <fjca185@gmail.com>
2024-05-10 11:59:49 -0600

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,74 @@
 import Image from "next/image";
-
+import { TECHNOLOGIES_CONSTANTS } from "./technologies-constants";
+type TechnologyImage = {
+  src: string;
+  height: number;
+  width: number;
+  alt: string;
+};
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="relative max-w-2xl m-auto my-16 flex flex-col gap-12 px-6 ">
+      <div className="scroll-m-20 text-2xl font-bold tracking-tight">
+        <h1 className="font-bold">Javier Cisneros</h1>
+        <h2 className="font-light">Software Development Engineer</h2>
+        <p className="font-normal text-base pt-2 text-justify">
+          I´m a Front-End software developer from Guadalajara, Jalisco. I
+          started studying software development when I was 16 years old in high
+          school and graduated as a junior software engineer. After that, I
+          started studying a bachelor degree in software development, and
+          graduated in June 2024.{" "}
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <p className="font-normal text-base pt-2 text-justify ali">
+          I´m passionate about learning and I´m always up to new challenges, I
+          want to become a better developer, and I´m always looking for new
+          opportunities to grow as a professional.{" "}
+        </p>
+        <h2 className="font-light pt-4">Contact</h2>
+        <div className="font-normal text-base pt-2 text-justify flex flex-wrap md:space-x-4 lg:space-x-4 sm:space-x-4 text-wrap">
+          <address className="underline not-italic w-full sm:w-auto">
+            <a href="mailto:'fjca185@gmail.com'">fjca185@gmail.com</a>
+          </address>
+          <p className="underline w-full sm:w-auto">
+            <a href="https://linkedin.com/in/javiercisnerosavila">Linkedln</a>
+          </p>
+          <p className="underline w-full sm:w-auto">
+            <a href="https://github.com/JavierCisneros">Github</a>
+          </p>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <h2 className="font-light pt-4">Technologies</h2>
+        <p className="font-normal text-base pt-2 text-justify">
+          Now I´m focusing on web development. I´m learning new technologies
+          such as React, Next.js, Tailwind CSS, and more. I´m also studying
+          software architecture, design patterns, and best practices in software
+          development. However, I´ve been working with these technologies for
+          the past few years.{" "}
+        </p>
+        <div className="flex flex-wrap justify-center p-4">
+          {Object.values(TECHNOLOGIES_CONSTANTS).map(
+            (technology: TechnologyImage) => (
+              <div
+                key={technology.src}
+                className="m-2 w-12 sm:w-10 md:w-10 lg:w-8 xl:w-10"
+              >
+                <Image
+                  src={technology.src}
+                  height={technology.height}
+                  width={technology.width}
+                  alt={technology.alt}
+                  className="w-full h-auto"
+                />
+              </div>
+            )
+          )}
+        </div>
+        <h2 className="font-light pt-4">Projects</h2>
+        <p className="font-normal text-base pt-2 text-justify">
+          I´ve been working on different projects. Some of them are personal
+          projects, and others are projects that I´ve been working on with other
+          developers.
+        </p>
       </div>
     </main>
   );
---
+++ src/app/technologies-constants.ts
@@ -0,0 +1,62 @@
+export const TECHNOLOGIES_CONSTANTS = {
+  IMAGE_1: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    height: 30,
+    width: 42,
+    alt: "javascript logo",
+  },
+  IMAGE_2: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    height: 30,
+    width: 42,
+    alt: "typescript logo",
+  },
+  IMAGE_3: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    height: 30,
+    width: 42,
+    alt: "html5 logo",
+  },
+  IMAGE_4: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    height: 30,
+    width: 42,
+    alt: "css3 logo",
+  },
+  IMAGE_5: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
+    height: 30,
+    width: 42,
+    alt: "tailwind logo",
+  },
+  IMAGE_6: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    height: 30,
+    width: 42,
+    alt: "react logo",
+  },
+  IMAGE_7: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+    height: 30,
+    width: 42,
+    alt: "next.js logo",
+  },
+  IMAGE_8: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg",
+    height: 30,
+    width: 42,
+    alt: "mysql logo",
+  },
+  IMAGE_9: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg",
+    height: 30,
+    width: 42,
+    alt: "android studio logo",
+  },
+  IMAGE_10: {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+    height: 30,
+    width: 42,
+    alt: "java logo",
+  },
+} as const;
