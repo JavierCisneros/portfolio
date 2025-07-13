1ff728685b9ef3a06818fda2efd40a4ae17c020e
JavierCisneros <fjca185@gmail.com>
2025-07-13 14:34:46 -0600

    fix: Standardize HTML entity usage for apostrophes in personal introduction for consistent rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,18 +54,17 @@ export default function Home() {
             I have experience building multitenant systems, Progressive Web
             Apps, and custom tools using modern technologies like React,
             Next.js, TypeScript, and AWS. I enjoy solving complex architectural
-            challenges by delivering solutions that align with the
-            system`&apos;s needs and organizational context, leveraging
-            Infrastructure as Code (IaC) and system design principles. I also
-            focus on optimizing performance and streamlining deployment
-            workflows through CI/CD best practices, using platforms such as AWS
-            CodePipeline and Vercel.
+            challenges by delivering solutions that align with the system&apos;s
+            needs and organizational context, leveraging Infrastructure as Code
+            (IaC) and system design principles. I also focus on optimizing
+            performance and streamlining deployment workflows through CI/CD best
+            practices, using platforms such as AWS CodePipeline and Vercel.
           </p>
 
           <p>
-            I'm passionate about learning and constantly improving. I thrive on
-            new challenges and seek opportunities that allow me to grow both
-            technically and professionally.
+            I&apos;m passionate about learning and constantly improving. I
+            thrive on new challenges and seek opportunities that allow me to
+            grow both technically and professionally.
           </p>
         </div>
 
